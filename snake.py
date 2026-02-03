0945a502ee75685b1b3039249b520b5a46e172cd
R1stAMOR <mortazap53@gmail.com>
2026-02-03 14:12:49 +0430

    writing codes for controlling the snake direction.

--- snake.py
+++ snake.py
@@ -3,6 +3,11 @@ from turtle import Turtle
 positions = [(0, 0), (-20, 0), (-40, 0)]
 move_distance = 20
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
         self.snake_body = []
@@ -22,6 +27,20 @@ class Snake:
             y = self.snake_body[num - 1].ycor()
             self.snake_body[num].goto(x, y)
         self.head.forward(move_distance)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
 
 
 
